ebdaf4f12b4c2cee7c1c20e30cf9dcc560b0a086
ngnepist_sabi_agri <stephane.ngnepiepaye-wembe@sabi-agri.com>
2025-01-14 17:40:33 +0100

    [LANDING_PAGE]: configuee the backgroung image

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,16 @@ const App = () => {
     <div className="app-container">
       <div className="hero">
         <div className="hero-left">
-          <h1>Welcome To LAMIFA</h1>
+          <h1>Welcome To laMIFA</h1>
           <div className="underline"></div>
           <p>It is important to know your origin</p>
           <button className="cta-button">Get Started</button>
         </div>
         <div className="hero-right">
-          <h2>Welcome to Paradise Nursery, where green meets serenity!</h2>
+          <h2>Welcome to laMIFA: here, family comes first!</h2>
           <p>
-            At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is
-            to provide a wide range of high-quality plants that not only enhance the beauty of your
-            surroundings but also contribute to a healthier and more sustainable lifestyle. From
-            air-purifying plants to aromatic fragrant ones, we have something for every plant enthusiast.
-          </p>
-          <p>
-            Our team of experts is dedicated to ensuring that each plant meets our strict standards of
-            quality and care. Whether you're a seasoned gardener or just starting your green journey,
-            we're here to support you every step of the way. Feel free to explore our collection, ask
-            questions, and let us help you find the perfect plant for your home or office.
-          </p>
-          <p>
-            Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today
-            and experience the beauty of nature right at your doorstep.
+          This web application allows you to create, save, and share your family tree with ease and security. 
+          With laMIFA, you can pass down a precious legacy to future generations, preserving your family’s history and memories.
           </p>
         </div>
       </div>
