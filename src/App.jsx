3498334e6480b72035e821a2a1b713a31b639393
ngnepist_sabi_agri <stephane.ngnepiepaye-wembe@sabi-agri.com>
2025-01-14 17:04:18 +0100

    [CONFIGURATION]: first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="hero">
+      <div className="hero-left">
+        <h1>Welcome To Paradise Nursery</h1>
+        <div className="underline"></div>
+        <p>Where Green Meets Serenity</p>
+        <button className="cta-button">Get Started</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="hero-right">
+        <h2>Welcome to Paradise Nursery, where green meets serenity!</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is
+          to provide a wide range of high-quality plants that not only enhance the beauty of your
+          surroundings but also contribute to a healthier and more sustainable lifestyle. From
+          air-purifying plants to aromatic fragrant ones, we have something for every plant enthusiast.
+        </p>
+        <p>
+          Our team of experts is dedicated to ensuring that each plant meets our strict standards of
+          quality and care. Whether you're a seasoned gardener or just starting your green journey,
+          we're here to support you every step of the way. Feel free to explore our collection, ask
+          questions, and let us help you find the perfect plant for your home or office.
+        </p>
+        <p>
+          Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today
+          and experience the beauty of nature right at your doorstep.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
